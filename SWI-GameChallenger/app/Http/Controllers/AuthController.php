c31bc06b64868d9dd2d8d48209892343b4f66188
Martin Bobčík <bobcik.martin@seznam.cz>
2017-09-07 13:41:45 +0200

    auth controller

---
+++ SWI-GameChallenger/app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function signin()
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        // Initialize the OAuth client
+        $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
+            'clientId'                => env('OAUTH_APP_ID'),
+            'clientSecret'            => env('OAUTH_APP_PASSWORD'),
+            'redirectUri'             => env('OAUTH_REDIRECT_URI'),
+            'urlAuthorize'            => env('OAUTH_AUTHORITY').env('OAUTH_AUTHORIZE_ENDPOINT'),
+            'urlAccessToken'          => env('OAUTH_AUTHORITY').env('OAUTH_TOKEN_ENDPOINT'),
+            'urlResourceOwnerDetails' => '',
+            'scopes'                  => env('OAUTH_SCOPES')
+        ]);
+
+        // Output the authorization endpoint
+        echo 'Auth URL: '.$oauthClient->getAuthorizationUrl();
+        exit();
+    }
+}
--- SWI-GameChallenger/app/Http/Controllers/Controller.php
+++ SWI-GameChallenger/app/Http/Controllers/Controller.php
@@ -11,3 +11,5 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
+
+Route::get('/signin', 'AuthController@signin');
