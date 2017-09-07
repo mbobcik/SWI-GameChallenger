6edbfb93c86d92d45cd276e0758e61bf9826ad24
Unknown <matusburzala@gmail.com>
2017-09-07 15:20:17 +0200

    neco

--- SWI-GameChallenger/config/database.php
+++ SWI-GameChallenger/config/database.php
@@ -41,14 +41,14 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '10.140.46.167'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'game_chellenger_db'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => '',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
